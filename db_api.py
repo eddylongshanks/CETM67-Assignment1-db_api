984568d1322dc162e63714400866f9374627eb0a
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-06-25 17:10:28 +0100

    added verbose responses and better logging

--- db_api.py
+++ db_api.py
@@ -3,7 +3,6 @@
 
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse, abort
-from decimal import Decimal
 import json
 import requests
 import boto3
@@ -23,7 +22,13 @@ class GetAllEnquiries(Resource):
             table = dynamodb.Table('Enquiry')
             enquiries = table.scan()
             return enquiries['Items']
-        except:
+
+        except Exception as e:
+            log(e)
+            return {
+                'statusCode': 500,
+                'body': json.dumps(str(e))
+            }
             raise
 
 
@@ -31,6 +36,7 @@ class AddEnquiry(Resource):
     def post(self):
         try:
             data = json.loads(request.data)
+            # Generate an ID
             data['id'] = get_guid()
             log(data)
 
@@ -39,10 +45,17 @@ class AddEnquiry(Resource):
                 Item = data
             )
 
-            return 'Enquiry added'
+            return {
+                'statusCode': 201,
+                'body': json.dumps(data)
+            }
 
         except Exception as e:
             log(e)
+            return {
+                'statusCode': 500,
+                'body': json.dumps(str(e))
+            }
             raise
 
 
@@ -58,18 +71,29 @@ class AddEnquirySNS(Resource):
 
             if header == 'Notification':
                 enquiry = process_sns(data)
+                log("Ready Enquiry: " + str(enquiry))
 
-            log(enquiry)
-
-            table = dynamodb.Table('Enquiry')
-            table.put_item(
-                Item = enquiry
-            )
+                table = dynamodb.Table('Enquiry')
+                table.put_item(
+                    Item = enquiry
+                )
             
-            return enquiry
+                return {
+                    'statusCode': 201,
+                    'body': json.dumps(enquiry)
+                }
+            else:
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps(data)
+                }
 
         except Exception as e:
             log(e)
+            return {
+                'statusCode': 500,
+                'body': json.dumps(str(e))
+            }
             raise
 
 
@@ -81,7 +105,10 @@ class GetLog(Resource):
 
 class HealthCheck(Resource):
     def get(self):
-        return 'DB API Available'
+        return {
+            'statusCode': 200,
+            'body': 'DB API Available'
+        }
 
 ## Routing ##
 api.add_resource(HealthCheck, '/')
@@ -94,7 +121,7 @@ api.add_resource(AddEnquirySNS, '/add-enquiry-sns')
 
 def process_sns(msg):
     # Converts the contents of the message string to a dictionary object
-    js = json.loads(msg['Message'], parse_float=Decimal)
+    js = json.loads(msg['Message'])
     js['id'] = get_guid()
 
     return js
@@ -105,9 +132,10 @@ def get_guid():
 
 def log(data_to_save):
     # Logs data to a local file for debugging
-    f = open('db_log.txt', 'w')
-    f.write(str(data_to_save))
-    f.close()
+    with open('db_log.txt', 'w') as log_file:
+        log_file.write(str(data_to_save))
+        log_file.write("\n")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
